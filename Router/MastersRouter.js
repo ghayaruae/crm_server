83b2b8b2bb7f818433c29c2d7dfe1eddb9cdc940
Habib Abubakar <habibabubakar736@gmail.com>
2025-10-31 16:23:15 +0530

    remove one master

--- Router/MastersRouter.js
+++ Router/MastersRouter.js
@@ -5,10 +5,10 @@ const AuthMiddleware = require("../Middleware/AuthMiddleware");
 const TargetController = require("../Controller/Masters/TargetController")
 
 /// Target routing
-router.post("/Masters/CreateTarget", AuthMiddleware.AdminAuth, TargetController.CreateTarget)
-router.get("/Masters/GetTargets", AuthMiddleware.AdminAuth, TargetController.GetTargets)
-router.get("/Masters/GetTargetInfo", AuthMiddleware.AdminAuth, TargetController.GetTargetInfo)
-router.post("/Masters/DeleteTarget", AuthMiddleware.AdminAuth, TargetController.DeleteTarget)
-router.get("/Masters/GetSalesmanList", AuthMiddleware.AdminAuth, TargetController.GetSalesmanList)
+router.post("/CreateTarget", AuthMiddleware.AdminAuth, TargetController.CreateTarget)
+router.get("/GetTargets", AuthMiddleware.AdminAuth, TargetController.GetTargets)
+router.get("/GetTargetInfo", AuthMiddleware.AdminAuth, TargetController.GetTargetInfo)
+router.post("/DeleteTarget", AuthMiddleware.AdminAuth, TargetController.DeleteTarget)
+router.get("/GetSalesmanList", AuthMiddleware.AdminAuth, TargetController.GetSalesmanList)
 
 module.exports = router;
