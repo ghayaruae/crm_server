c469bd29f24f0b62712a648c2a7a9d5d4d25bf78
YasirSayyed07 <yasirdualsysco07@gmail.com>
2025-11-01 19:26:25 +0530

    refactor: simplify static file serving logic in index.js

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true, }));
 
 app.use(cors({ origin: '*' }));
-// app.use('/public', express.static(path.join(__dirname, 'public')));
-const publicPath = path.join(__dirname, 'public');
-app.use('/public', express.static(publicPath));
-
-// Add this to verify the static path
-console.log('Static files served from:', publicPath);
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get("/", (req, res) => { res.json("Working") });
 
 
